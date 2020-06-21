31b59db2fad909f68ffa320e51c5a8937476f9e0
Garrett Trott <garretttrott@gmail.com>
2020-06-21 02:00:25 -0700

    moved state to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,37 @@
-import React from 'react'
-
+import React, {Component} from 'react'
 import Header from './Header'
 import Player from './Player'
-import Counter from './Counter'
 
-class App extends React.Component {
+class App extends Component {
   state = {
     players: [
       {
         name: 'Guil',
+        score: 0,
         id: 1,
       },
       {
         name: 'Treasure',
+        score: 0,
         id: 2,
       },
       {
         name: 'Ashley',
+        score: 0,
         id: 3,
       },
       {
         name: 'James',
+        score: 0,
         id: 4,
       },
     ],
+  }
+
+  handelScoreChange = (index, delta) => {
+    this.setState((prevState) => ({
+      score: (prevState.players[index].score += delta),
+    }))
   }
 
   handleRemovePlayer = (id) => {
@@ -40,11 +48,14 @@ class App extends React.Component {
         <Header title="Scoreboard" totalPlayers={this.state.players.length} />
 
         {/* Players list */}
-        {this.state.players.map((player) => (
+        {this.state.players.map((player, index) => (
           <Player
+            score={player.score}
             name={player.name}
             id={player.id}
             key={player.id.toString()}
+            index={index}
+            changeScore={this.handelScoreChange}
             removePlayer={this.handleRemovePlayer}
           />
         ))}
