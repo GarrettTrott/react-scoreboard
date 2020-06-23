d63a84aa16e55979b15a66791597eac0766c10d6
Garrett Trott <garretttrott@gmail.com>
2020-06-22 19:27:03 -0700

    add tick and handel stopwatch functions

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
 
 export class Stopwatch extends Component {
+  state = {
+    isRunning: false,
+    elapsedTime: 0,
+    previousTime: 0,
+  }
+
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 100)
+  }
+
+  tick = () => {
+    if (this.state.isRunning) {
+      const now = Date.now()
+      this.setState((prevState) => ({
+        previousTime: now,
+        elapsedTime:
+          now - prevState.elapsedTime + (now - this.state.previousTime),
+      }))
+    }
+  }
+
+  handelStopwatch = () => {
+    this.setState((prevState) => ({
+      isRunning: !prevState.isRunning,
+    }))
+    if (!this.state.isRunning) {
+      this.setState({ previousTime: Date.now() })
+    }
+  }
+
   render() {
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
         <span className="stopwatch-time">0</span>
-        <button>Start</button>
+        <button onClick={this.handelStopwatch}>
+          {this.state.isRunning ? 'Stop' : 'start'}
+        </button>
         <button>Reset</button>
       </div>
     )
